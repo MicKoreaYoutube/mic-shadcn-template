06dcb3de06377a76e8d1d51faa20cf9d30fe2279
leejunsibal <118183460+MicKoreaYoutube@users.noreply.github.com>
2025-08-18 09:22:21 +0000

    안 되겠지... 그래도 theme 알아내서 기분 좋음!!

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 
 import { buttonVariants } from "@/components/ui/button"
-import { IntersectionObserverComp } from "@/components/util-components"
+import { IntersectionObserverComp, ReverseTheme } from "@/components/util-components"
 
 export default function Page() {
   const classToAdd = "animate__animated animate__fadeIn"
+
   return (
     <>
       <div className="bg-background flex flex-col gap-2 p-8 md:p-10">
@@ -29,26 +30,28 @@ export default function Page() {
           </Link>
         </div>
       </div>
-      <div className="bg-foreground flex flex-col gap-2 p-8 md:p-10">
-        <IntersectionObserverComp triggerOnce={true} threshold={1} originalClass="invisible" addedClass={classToAdd}>
-          <h1 className="font-KBODiaGothic_bold text-background text-3xl font-extrabold tracking-tighter md:text-4xl">
-            Beautifully designed components <br className="hidden sm:inline" />
-            built with Radix UI and Tailwind CSS.
-          </h1>
-        </IntersectionObserverComp>
-        <p className="font-SUITE_Regular text-muted-foreground text-lg">
-          Accessible and customizable components that you can copy and paste into your apps. Free. Open Source. And
-          Next.js 13 Ready.
-        </p>
-        <div className="font-TheJamsil5Bold flex gap-4">
-          <Link href={siteConfig.links.shadcnuiDocs} target="_blank" className={buttonVariants()}>
-            Documentation
-          </Link>
-          <Link target="_blank" href={siteConfig.links.micGithub} className={buttonVariants({ variant: "outline" })}>
-            GitHub
-          </Link>
+      <ReverseTheme>
+        <div className="bg-background text-foreground flex flex-col gap-2 p-8 md:p-10">
+          <IntersectionObserverComp triggerOnce={true} threshold={1} originalClass="invisible" addedClass={classToAdd}>
+            <h1 className="font-KBODiaGothic_bold text-3xl font-extrabold tracking-tighter md:text-4xl">
+              Beautifully designed components <br className="hidden sm:inline" />
+              built with Radix UI and Tailwind CSS.
+            </h1>
+          </IntersectionObserverComp>
+          <p className="font-SUITE_Regular text-muted-foreground text-lg">
+            Accessible and customizable components that you can copy and paste into your apps. Free. Open Source. And
+            Next.js 13 Ready.
+          </p>
+          <div className="font-TheJamsil5Bold flex gap-4">
+            <Link href={siteConfig.links.shadcnuiDocs} target="_blank" className={buttonVariants()}>
+              Documentation
+            </Link>
+            <Link target="_blank" href={siteConfig.links.micGithub} className={buttonVariants({ variant: "outline" })}>
+              GitHub
+            </Link>
+          </div>
         </div>
-      </div>
+      </ReverseTheme>
       <div className="bg-background flex flex-col gap-2 p-8 md:p-10">
         <IntersectionObserverComp triggerOnce={true} threshold={1} originalClass="invisible" addedClass={classToAdd}>
           <h1 className="font-KBODiaGothic_bold text-3xl font-extrabold tracking-tighter md:text-4xl">
@@ -69,26 +72,28 @@ export default function Page() {
           </Link>
         </div>
       </div>
-      <div className="bg-foreground flex flex-col gap-2 p-8 md:p-10">
-        <IntersectionObserverComp triggerOnce={true} threshold={1} originalClass="invisible" addedClass={classToAdd}>
-          <h1 className="font-KBODiaGothic_bold text-background text-3xl font-extrabold tracking-tighter md:text-4xl">
-            Beautifully designed components <br className="hidden sm:inline" />
-            built with Radix UI and Tailwind CSS.
-          </h1>
-        </IntersectionObserverComp>
-        <p className="font-SUITE_Regular text-muted-foreground text-lg">
-          Accessible and customizable components that you can copy and paste into your apps. Free. Open Source. And
-          Next.js 13 Ready.
-        </p>
-        <div className="font-TheJamsil5Bold flex gap-4">
-          <Link href={siteConfig.links.shadcnuiDocs} target="_blank" className={buttonVariants()}>
-            Documentation
-          </Link>
-          <Link target="_blank" href={siteConfig.links.micGithub} className={buttonVariants({ variant: "outline" })}>
-            GitHub
-          </Link>
+      <ReverseTheme>
+        <div className="bg-background text-foreground flex flex-col gap-2 p-8 md:p-10">
+          <IntersectionObserverComp triggerOnce={true} threshold={1} originalClass="invisible" addedClass={classToAdd}>
+            <h1 className="font-KBODiaGothic_bold text-3xl font-extrabold tracking-tighter md:text-4xl">
+              Beautifully designed components <br className="hidden sm:inline" />
+              built with Radix UI and Tailwind CSS.
+            </h1>
+          </IntersectionObserverComp>
+          <p className="font-SUITE_Regular text-muted-foreground text-lg">
+            Accessible and customizable components that you can copy and paste into your apps. Free. Open Source. And
+            Next.js 13 Ready.
+          </p>
+          <div className="font-TheJamsil5Bold flex gap-4">
+            <Link href={siteConfig.links.shadcnuiDocs} target="_blank" className={buttonVariants()}>
+              Documentation
+            </Link>
+            <Link target="_blank" href={siteConfig.links.micGithub} className={buttonVariants({ variant: "outline" })}>
+              GitHub
+            </Link>
+          </div>
         </div>
-      </div>
+      </ReverseTheme>
     </>
   )
 }
--- components/util-components.tsx
+++ components/util-components.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useRouter } from "next/navigation"
+import { useTheme } from "next-themes"
 import React, { ReactElement, ReactNode } from "react"
 
 import { InView, IntersectionObserverProps, useInView } from "react-intersection-observer"
@@ -9,16 +10,20 @@ import { Button, ButtonProps } from "@/components/ui/button"
 
 import { cn } from "@/lib/utils"
 
-function BackwardButton(props: ButtonProps) {
-  const router = useRouter()
-
-  return <Button rel="noreferrer" onClick={() => router.back()} {...props} />
-}
-
 interface IntersectionObserverCompProps extends Omit<IntersectionObserverProps, "children"> {
   originalClass?: string
   addedClass?: string
   children?: ReactNode
+}
+
+interface ReverseThemeProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string
+}
+
+function BackwardButton(props: ButtonProps) {
+  const router = useRouter()
+
+  return <Button rel="noreferrer" onClick={() => router.back()} {...props} />
 }
 
 function IntersectionObserverComp({ originalClass, addedClass, children, ...props }: IntersectionObserverCompProps) {
@@ -33,4 +38,14 @@ function IntersectionObserverComp({ originalClass, addedClass, children, ...prop
   )
 }
 
-export { BackwardButton, IntersectionObserverComp }
+function ReverseTheme({ className, children }: ReverseThemeProps) {
+  const { theme } = useTheme()
+
+  return (
+    <div className={cn(theme == "dark" ? "light" : "dark", className)}>
+      {children}
+    </div>
+  )
+}
+
+export { BackwardButton, IntersectionObserverComp, ReverseTheme }
